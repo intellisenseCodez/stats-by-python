6a7f7ccd9777adeba67a7d8763112e9b2731b320
Hadexsnow <adeyemionabanjo3@gmail.com>
2025-04-08 11:32:14 -0400

    changes made please check

--- descriptive.py
+++ descriptive.py
@@ -1,1 +1,9 @@
-def mean(numbers)
+def median(numbers):
+    sorted_numbers = sorted(numbers)
+    if len(sorted_numbers) % 2 != 0:
+        index_pos = len(sorted_numbers) // 2
+        median_value = sorted_numbers[index_pos]
+    
+    return median_value
+
+
